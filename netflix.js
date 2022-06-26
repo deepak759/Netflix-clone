79c0064419a3bfbb0e0514199f9363640a591c4d
deepak759 <83724070+deepak759@users.noreply.github.com>
2022-06-26 14:44:21 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+function mysigninform(){
+    document.getElementById("signinform").innerHTML+="<h1>hgfhdgfgjdf</h1>"
+
+    
+ }
